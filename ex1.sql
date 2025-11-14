79d386eb2e9153fc213c4b29b69dc2e7a0545f36
Lorena Lira <naheelee2005@gmail.com>
2025-11-14 16:28:42 -0300

    Create Clinica database schema with tables and indexes
    Created a database schema for Clinica with tables for Ambulatorios, Medicos, Pacientes, Funcionarios, and Consultas. Added indexes and made alterations to the Funcionarios table.

---
+++ ex1.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE Clinica;
+
+USE Clinica;
+
+CREATE TABLE Ambulatorios (
+nroa int primary key ,
+andar numeric(3) not null,
+capacidade smallint
+);
+
+CREATE TABLE Medicos (
+codm int primary key ,
+nome varchar(40) not null,
+idade smallint not null,
+especialidade char(20),
+CPF numeric(11) unique,
+cidade varchar(30),
+nroa int,
+foreign key (nroa) references Ambulatorios (nroa)
+);
+
+CREATE TABLE Pacientes (
+codp int primary key,
+nome varchar(40) not null,
+idade smallint not null,
+cidade char(30),
+CPF numeric (11) unique,
+doenca varchar(40) not null
+);
+
+CREATE TABLE Funcionarios(
+codf int primary key,
+nome varchar(40) not null,
+idade smallint,
+CPF numeric (11) unique,
+cidade char(30),
+salario numeric(10),
+cargo varchar(20)
+);
+
+CREATE TABLE Consultas(
+codm int primary key,
+codp int,
+data date,
+horario time,
+foreign key (codp) references Pacientes (codp)
+);
+
+alter table Funcionarios add column (nroa int); 
+
+create unique index idx_med_cpf on Medicos(CPF);
+
+create index idx_pac_doenca on Pacientes(doenca);
+
+drop index idx_pac_doenca on Pacientes;
+
+alter table Funcionarios drop column cargo, drop column nroa;
